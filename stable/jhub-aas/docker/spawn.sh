fcc0f2949011de60877b1ce82cf23a2dabcd81c9
ciangottini <ciangottini@pg.infn.it>
2021-09-21 11:24:25 +0200

    update jlab image and

--- stable/jhub-aas/docker/spawn.sh
+++ stable/jhub-aas/docker/spawn.sh
@@ -8,7 +8,7 @@ oidc-gen dodas --issuer $IAM_SERVER \
                --client-secret $IAM_CLIENT_SECRET \
                --rt $REFRESH_TOKEN \
                --confirm-yes \
-               --scope "openid profile email" \
+               --scope "openid profile email wlcg wlcg.groups" \
                --redirect-uri  http://localhost:8843 \
                --pw-cmd "echo \"DUMMY PWD\""
 
