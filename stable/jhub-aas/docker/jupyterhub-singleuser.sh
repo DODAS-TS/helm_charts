939ca1bb63692cd0e3049d94e35f62bf68cf76fe
ciangottini <ciangottini@pg.infn.it>
2021-09-23 23:22:40 +0200

    root overwrite jupyterPATH

--- stable/jhub-aas/docker/jupyterhub-singleuser.sh
+++ stable/jhub-aas/docker/jupyterhub-singleuser.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
-#conda activate cms-dodas
+source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
+conda activate cms-dodas
 
 export PYTHONPATH=/opt/conda/lib/python3.9/site-packages/:$PYTHONPATH
 
 source /cvmfs/cms.dodas.infn.it/miniconda3/envs/cms-dodas/bin/thisroot.sh
+export JUPYTER_PATH=/opt/conda/etc/jupyter
+export JUPYTER_CONFIG_DIR=/opt/conda/etc/jupyter
 
 python /usr/local/bin/jupyterhub-singleuser "$@"
