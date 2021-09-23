5e2abf206ebbd96c10266538df16a0d6b232217d
ciangottini <ciangottini@pg.infn.it>
2021-09-23 23:13:30 +0200

    import only root

--- stable/jhub-aas/docker/jupyterhub-singleuser.sh
+++ stable/jhub-aas/docker/jupyterhub-singleuser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
-conda activate cms-dodas
+#source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
+#conda activate cms-dodas
 
 export PYTHONPATH=/opt/conda/lib/python3.9/site-packages/:$PYTHONPATH
 
