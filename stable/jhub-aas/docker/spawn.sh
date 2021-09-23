93e28b14b27f602eb43ca7c61340212becb03e4e
ciangottini <ciangottini@pg.infn.it>
2021-09-23 09:50:06 +0200

    fix spawn

--- stable/jhub-aas/docker/spawn.sh
+++ stable/jhub-aas/docker/spawn.sh
@@ -14,5 +14,7 @@ oidc-gen dodas --issuer $IAM_SERVER \
 
 while true; do oidc-token dodas --time 1200 > /tmp/token; sleep 600; done &
 
-source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
-conda activate cms-dodas
+if [[ -f "cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh" ]]; then
+    source /cvmfs/cms.dodas.infn.it/miniconda3/etc/profile.d/conda.sh
+    conda activate cms-dodas
+fi
